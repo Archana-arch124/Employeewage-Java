4485e9977176936c385201e984515866d760c7a6
unknown <#archgowda124@gmail.com>
2020-12-03 11:48:28 +0530

    Refactored:checks Employee attendance

--- employeeWageCalculation.java
+++ employeeWageCalculation.java
@@ -1,2 +1,19 @@
-Welcome to Employee wage problem calculation in java
+//Welcome to Employee wage problem calculation in java
 
+public class EmployeeWageComputation
+{
+    public static void main(String args[])
+    {
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
+    }
+}
